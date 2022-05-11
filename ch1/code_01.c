d4310d59ffde6adc6304edbc546d176ce58d05c4
Zhuomin Lin <98099819+1ZHUOMIN@users.noreply.github.com>
2022-05-11 21:40:58 +0800

    Update and rename ch2/code_01.c to ch1/code_01.c

---
+++ ch1/code_01.c
@@ -0,0 +1,13 @@
+#选择语句
+#include <stdio.h>
+int main()
+{
+    int input = 0;
+    printf("study? 1 or 0 =>");
+    scanf("%d",&input);
+    if(input==1)
+        printf("yes!");
+    else
+        printf("no!");
+    return 0;
+}
