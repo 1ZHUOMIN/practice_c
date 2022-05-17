477dec0d8bb321133334248ee6882c40289fede4
Zhuomin Lin <98099819+mminminn@users.noreply.github.com>
2022-05-17 21:30:34 +0800

    Update static.c

--- ch1/static.c
+++ ch1/static.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 void test()
 {
-    //int a = 1;
-    static int a = 1; //使a变为静态的局部变量，也就是保存上一次得到的a
+    //int a = 1; //若不用static，那么每次函数进入循环出来后，a都会变回为1，输出：2 2 2 2 2 
+    static int a = 1; //使a变为静态的局部变量，也就是保存上一次得到的a， 输出：2 3 4 5 6
     a++;
     printf("%d\n", a);
 }
