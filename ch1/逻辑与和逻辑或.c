d23f487e72baec227a9740d3db3aebbbeb69c0ff
Zhuomin Lin <98099819+mminminn@users.noreply.github.com>
2022-05-17 09:53:43 +0800

    Update 逻辑与和逻辑或.c

--- ch1/逻辑与和逻辑或.c
+++ ch1/逻辑与和逻辑或.c
@@ -3,8 +3,8 @@ int main()
 {
     int a = 0; //0
     int b = 5; //非0
-    int c = a&&b;
-    int d = a||b;
+    int c = a&&b; //逻辑与&&
+    int d = a||b; //逻辑或||
     printf("%d\n%d\n", c, d);
     return 0;
 } 
