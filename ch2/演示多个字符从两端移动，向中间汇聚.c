12c159919afa6a0d5cc7c5b038d76e545c71b59d
Zhuomin Lin <98099819+mminminn@users.noreply.github.com>
2022-05-25 16:34:12 +0800

    Update 演示多个字符从两端移动，向中间汇聚.c

--- ch2/演示多个字符从两端移动，向中间汇聚.c
+++ ch2/演示多个字符从两端移动，向中间汇聚.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <string.h>  //strlen从这来
 #include <windows.h> //sleep从这来
+#include <stdlib.h>  //system从这来 
 int main()
 {
     char arr1[] = "welcome to bit!!!!!!";
@@ -21,6 +22,7 @@ int main()
         arr2[right] = arr1[right];
         printf("%s\n", arr2);
         Sleep(1000); //休息1秒
+        system("cls"); //执行系统命令的一个函数-cls-清空屏幕
         left++;
         right--;
     }
